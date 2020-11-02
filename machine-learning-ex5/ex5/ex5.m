810015b534e2f8b21e35c8dbe8cc5b8d81e34186
Ralph Su <suliangfei@gmail.com>
2020-11-01 21:25:38 -0700

    init of ex7

--- machine-learning-ex5/ex5/ex5.m
+++ machine-learning-ex5/ex5/ex5.m
@@ -35,12 +35,12 @@ load ('ex5data1.mat');
 m = size(X, 1);
 
 % Plot training data
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
-xlabel('Change in water level (x)');
-ylabel('Water flowing out of the dam (y)');
+%plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+%xlabel('Change in water level (x)');
+%ylabel('Water flowing out of the dam (y)');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% =========== Part 2: Regularized Linear Regression Cost =============
 %  You should now implement the cost function for regularized linear 
@@ -86,15 +86,15 @@ lambda = 0;
 [theta] = trainLinearReg([ones(m, 1) X], y, lambda);
 
 %  Plot fit over the data
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
-xlabel('Change in water level (x)');
-ylabel('Water flowing out of the dam (y)');
-hold on;
-plot(X, [ones(m, 1) X]*theta, '--', 'LineWidth', 2)
-hold off;
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+%xlabel('Change in water level (x)');
+%ylabel('Water flowing out of the dam (y)');
+%hold on;
+%plot(X, [ones(m, 1) X]*theta, '--', 'LineWidth', 2)
+%hold off;
+%
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 
 %% =========== Part 5: Learning Curve for Linear Regression =============
@@ -110,12 +110,12 @@ lambda = 0;
                   [ones(size(Xval, 1), 1) Xval], yval, ...
                   lambda);
 
-plot(1:m, error_train, 1:m, error_val);
-title('Learning curve for linear regression')
-legend('Train', 'Cross Validation')
-xlabel('Number of training examples')
-ylabel('Error')
-axis([0 13 0 150])
+%plot(1:m, error_train, 1:m, error_val);
+%title('Learning curve for linear regression')
+%legend('Train', 'Cross Validation')
+%xlabel('Number of training examples')
+%ylabel('Error')
+%axis([0 13 0 150])
 
 fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
 for i = 1:m
